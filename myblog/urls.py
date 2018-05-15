a314384301c021601dc55da3a9ac785c0d3ce0a6
breakbadsp <sachinfbook1@gmail.com>
2018-05-15 18:26:59 +0530

    blank home page

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import urls
+from django.conf.urls import url
 from . import views
 
-urlpatters = [
-	url(r'^$', views.post_list, name='post_list')
+urlpatterns = [
+	url(r'^$', views.post_list, name='post_list'),
 ]
