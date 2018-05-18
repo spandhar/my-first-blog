0db23e4402d3303980ca3aed2baba737ed36672e
breakbadsp <sachinfbook1@gmail.com>
2018-05-18 19:07:40 +0530

    template added with query

--- myblog/views.py
+++ myblog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from myblog.models import Post
+from django.contrib.auth.models import User
 
 def post_list(request):
-    return render(request, 'myblog\post_list.html', {})
+	me = User.objects.get(username="breakbadsp")
+	posts = Post.objects.filter(author=me).order_by('published_date')
+	return render(request, 'myblog/post_list.html', {'posts':posts})
